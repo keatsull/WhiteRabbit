36edb4147a2f79752a4ee1a5cf07d50a892f0db0
Keaten <s3482226@student.rmit.edu.au>
2017-10-11 16:33:08 +1000

    added fade in

---
+++ js/fadeIn.js
@@ -0,0 +1,6 @@
+$(document).ready(function(e) {
+	$(document.body).delay(300).animate({'opacity': '1'}, 700);
+	$('.characterContainer').delay(500).animate({'opacity': '1'}, 700);
+	$('#conversationContainer').delay(700).animate({'opacity': '1'}, 700);
+	$('.optionContainer').delay(900).animate({'opacity': '1'}, 700);
+});
